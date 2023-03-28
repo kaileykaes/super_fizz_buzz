cb04a8a96317356046a1d6e74b7a3c8af0a4c20d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-28 12:42:53 -0600

    Test: #input_adjustment

--- spec/super_fizz_buzz_spec.rb
+++ spec/super_fizz_buzz_spec.rb
@@ -1,28 +1,36 @@
 require './lib/super_fizz_buzz'
 
 RSpec.describe SuperFizzBuzz do
-  before(:each) do
-    @super_fizz = SuperFizzBuzz.new
-  end
-
-  describe '#initialize'
+  describe '#initialize' do
     it 'exists' do
-      expect(@super_fizz).to be_a SuperFizzBuzz
+      super_fizz = SuperFizzBuzz.new
+      expect(super_fizz).to be_a SuperFizzBuzz
     end
   end
 
   describe '#output' do 
-    it 'outputs strings based on input' do 
-      expect(@super_fizz.output(8)).to eq('8')
-      expect(@super_fizz.output(15)).to eq('FizzBuzz')
+    it 'outputs based on input' do 
+      super_fizz = SuperFizzBuzz.new
+      expect(super_fizz.output(8)).to eq('8')
+      expect(super_fizz.output(15)).to eq('FizzBuzz')
     end
   end
 
   describe '#output_range' do 
     it 'outputs strings based on input' do 
-      expect(@super_fizz.output_range(8, 15)).to eq([
+      super_fizz = SuperFizzBuzz.new
+      expect(super_fizz.output_range(8, 15)).to eq([
         "8", "Fizz", "Buzz", "11", 
         "Fizz", "13", "Super", "FizzBuzz"
       ])
     end
   end
+
+  describe '#input_adjustment' do 
+    it 'adjusts input' do 
+      super_fizz = SuperFizzBuzz.new
+      expect(super_fizz.input_adjustment('1, 3')).to eq([1, 3])
+      expect(super_fizz.input_adjustment('3')).to eq(3)
+    end
+  end
+end
