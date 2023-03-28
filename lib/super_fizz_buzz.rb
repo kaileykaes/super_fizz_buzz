6d1b460701034f33fab3d8c0b7571edab13d5049
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-28 12:43:32 -0600

    Feat: #input_adjustment

--- lib/super_fizz_buzz.rb
+++ lib/super_fizz_buzz.rb
@@ -44,26 +44,21 @@ class SuperFizzBuzz
     end
     range_outputs
   end
-end
 
-# Iterate through the numbers 1 to 1000
-# For each one, print the output according to the rules of Super Fizz Buzz
-# 1.upto(1000).map do |num|
-#   if num % 5 == 0 && num % 3 == 0 && num % 7 == 0
-#     puts 'SuperFizzBuzz'
-#   elsif num % 3 == 0 && num % 7 == 0 && num % 5 != 0
-#     puts 'SuperFizz' 
-#   elsif num % 5 == 0 && num % 7 == 0 && num % 3 != 0 
-#     puts 'SuperBuzz' 
-#   elsif num % 3 == 0 && num % 5 == 0 && num % 7 != 0
-#     puts 'FizzBuzz'
-#   elsif num % 5 != 0 && num % 7 != 0 && num % 3 == 0 
-#     puts 'Fizz'
-#   elsif num % 7 != 0 && num % 3 != 0 && num % 5 == 0 
-#     puts 'Buzz'
-#   elsif num % 7 == 0 && num % 3 != 0 && num % 5 != 0
-#     puts 'Super'
-#   else
-#     puts num
-#   end
-# end
+
+#helpers
+
+  def input_adjustment(input)
+    # require 'pry'; binding.pry
+    if input.length == 1
+      input = input.to_i
+    elsif
+      input.length > 1
+      input = input.delete(',').split(' ')
+      inputs = input.map! do |ins|
+          ins.to_i
+      end
+      inputs
+    end
+  end
+end
